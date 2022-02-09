eb0c339540722437b6c61d96245211c4687f1a1c
Anthony Twesigye <twesigye10@gmail.com>
2022-02-09 11:58:20 +0300

    frequency_of_child_violence_occurrence_risk_witnessed_30

--- R/logical_checks_for_data_collection.R
+++ R/logical_checks_for_data_collection.R
@@ -666,3 +666,30 @@ if(exists("df_c_logic_frequency_unaccompanied_children_occurrence")){
     logic_output$df_c_logic_frequency_unaccompanied_children_occurrence <- df_c_logic_frequency_unaccompanied_children_occurrence
   }
 }
+# frequency_of_child_violence_occurrence_risk_witnessed_30
+df_c_logic_frequency_of_child_violence_occurrence_risk <- df_tool_data %>% 
+  filter(child_protection_risks_witnessed %in% c("physical_violence"),
+         frequency_of_child_violence_occurrence == "never") %>% 
+  mutate(i.check.type = "change_response",
+         i.check.name = "frequency_of_child_violence_occurrence",
+         i.check.current_value = frequency_of_child_violence_occurrence,
+         i.check.value = "",
+         i.check.issue_id = "frequency_of_child_violence_occurrence_risk_witnessed_30",
+         i.check.issue = glue("child_protection_risks_witnessed: {child_protection_risks_witnessed},  
+                              frequency_of_child_violence_occurrence: {frequency_of_child_violence_occurrence}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = paste0(i.check.uuid, "_", i.check.type, "_", i.check.name),
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check"))%>% 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+if(exists("df_c_logic_frequency_of_child_violence_occurrence_risk")){
+  if(nrow(df_c_logic_frequency_of_child_violence_occurrence_risk) > 0){
+    logic_output$df_c_logic_frequency_of_child_violence_occurrence_risk <- df_c_logic_frequency_of_child_violence_occurrence_risk
+  }
+}
