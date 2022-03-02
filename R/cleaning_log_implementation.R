56239838beadca8b1f670a4090f6f31044673237
Anthony Twesigye <twesigye10@gmail.com>
2022-03-02 08:53:01 +0300

    read data

---
+++ R/cleaning_log_implementation.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(lubridate)
+library(glue)
+
+
+# read data ---------------------------------------------------------------
+
+# tool data
+df_tool_data <- readxl::read_excel("inputs/UGA2109_Cross_Sectoral_Child_Protection_Assessment_Caregiver_Data.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         i.check.enumerator_id = enumerator_id,
+         i.check.district_name = district_name,
+         i.check.point_number = point_number,
+         start = as_datetime(start),
+         end = as_datetime(end)) %>% 
+  filter(consent_two == "yes", respondent_age >= 12, i.check.start_date > as_date("2022-01-30"), 
+         !str_detect(string = point_number, pattern = fixed('test', ignore_case = TRUE))
+  )
+# cleaning log
+df_cleaning_log <- read_csv("inputs/combined_checks_caregiver.csv") %>% 
+  mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log)) %>%
+  filter(adjust_log != "delete_log", !is.na(value), !is.na(uuid)) %>% 
+  mutate(sheet = NA, index = NA, relevant = NA) %>% 
+  select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
+# survey tool
+df_survey <- readxl::read_excel("inputs/Child_Protection_Assessment_Caregiver_Tool.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/Child_Protection_Assessment_Caregiver_Tool.xlsx", sheet = "choices")
