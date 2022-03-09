e8ee4dc3a70cb6fd120e6bb4cbaf7f2839bbc26c
Anthony Twesigye <twesigye10@gmail.com>
2022-03-09 16:42:47 +0300

    fixing region

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -3,10 +3,12 @@
 create_composite_indicators_dfa <- function(input_df) {
   input_df %>% 
     mutate(
-      i.refugee_settlement = case_when(district_name == "adjumani" & status == "refugee"~ "adjumani", 
+      i.refugee_settlement = case_when(district_name == "adjumani" & status == "refugee" ~ "adjumani", 
                                        settlement_name == "rhino" ~ "rhino_camp",
                                        TRUE ~ settlement_name),
-      i.region = ifelse(district_name %in% c("isingiro", "kamwenge", "kikuube", "kyegegwa"), "south_west", "west_nile")
+      i.region = case_when(district_name %in% c("kampala") ~ "central",
+                           district_name %in% c("isingiro", "kamwenge", "kikuube", "kyegegwa") ~ "south_west"
+                           TRUE ~ "west_nile")
     ) %>% 
     select(-starts_with("int."))
 }
