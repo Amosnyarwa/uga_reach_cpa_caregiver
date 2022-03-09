d0718d842cb8e4cc11e7e60f347a710e26c86186
Anthony Twesigye <twesigye10@gmail.com>
2022-03-09 16:47:45 +0300

    load data

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -6,3 +6,12 @@ library(glue)
 # source some scripts
 source("R/composite_indicators.R")
 source("R/make_weights.R")
+
+# load data ---------------------------------------------------------------
+
+df_cleaned <- read_csv("inputs/clean_data_caregiver.csv")
+
+dap <- read_csv("inputs/r_dap.csv") %>% 
+  janitor::clean_names()
+
+start<- Sys.time() 
