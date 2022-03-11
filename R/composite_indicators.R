6f7919bbb1381f5e7247112f46805d5c09b86adf
Anthony Twesigye <twesigye10@gmail.com>
2022-03-11 14:11:48 +0300

    modified the refugee_settlement column name

--- R/composite_indicators.R
+++ R/composite_indicators.R
@@ -4,8 +4,8 @@ create_composite_indicators_cpa_caregiver <- function(input_df) {
   input_df %>% 
     mutate(
       i.refugee_settlement = case_when(district_name == "adjumani" & status == "refugee" ~ "adjumani", 
-                                       settlement_name == "rhino" ~ "rhino_camp",
-                                       TRUE ~ settlement_name),
+                                       refugee_settlement == "rhino" ~ "rhino_camp",
+                                       TRUE ~ refugee_settlement),
       i.region = case_when(district_name %in% c("kampala") ~ "central",
                            district_name %in% c("isingiro", "kamwenge", "kikuube", "kyegegwa") ~ "south_west"
                            TRUE ~ "west_nile"),
