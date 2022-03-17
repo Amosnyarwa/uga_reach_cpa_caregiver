d1fb417ba6971f7163def52cc78d4979fc014f95
Anthony Twesigye <twesigye10@gmail.com>
2022-03-17 17:23:09 +0300

    read correct file

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -14,7 +14,7 @@ df_children_perform_domestic_chores_info = readxl::read_excel(path = "inputs/UGA
 df_protection_risky_places = readxl::read_excel(path = "inputs/UGA2109_Cross_Sectoral_Child_Protection_Assessment_Caregiver_Data.xlsx", sheet = "protection_risky_places")
 df_children_perform_economic_labour_info = readxl::read_excel(path = "inputs/UGA2109_Cross_Sectoral_Child_Protection_Assessment_Caregiver_Data.xlsx", sheet = "children_perform_economic_labour_info")
 
-dap <- read_csv("inputs/r_dap.csv") %>% 
+dap <- read_csv("inputs/r_dap_caregiver.csv") %>% 
   janitor::clean_names()
 
 start<- Sys.time() 
