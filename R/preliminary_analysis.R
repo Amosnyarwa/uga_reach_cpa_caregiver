e89e36eac87b00e2a1385f2b9246efb39853593d
Anthony Twesigye <twesigye10@gmail.com>
2022-04-06 09:46:11 +0300

    protection_risky_places: prepare data and create survey

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -97,43 +97,24 @@ df_children_perform_domestic_chores_info_analysis <- analysis_support_after_surv
 
 # protection_risky_places: prepare data and create survey ------------------------------------------------
 
-df_with_composites <- create_composite_indicators_cpa_caregiver(input_df = df_cleaned) %>% 
-  mutate(strata = case_when(status == "refugee" ~ paste0(i.refugee_settlement, "_refugee"),
-                            status == "host_community" ~ paste0(i.region,"_host"),
-                            TRUE ~ status
-  ))
+df_with_composites_protection_risky_places <- create_composite_indicators_cpa_caregiver(input_df = df_cleaned_protection_risky_places_data) 
 
 # split data into host and refugee
 
-df_ref <- df_with_composites %>% 
+df_ref_protection_risky_places <- df_with_composites_protection_risky_places %>% 
   filter(status == "refugee")
 
-df_host <- df_with_composites %>% 
+df_host_protection_risky_places <- df_with_composites_protection_risky_places %>% 
   filter(status == "host_community")
-
-# create weights
-
-# refugee weights
-ref_weight_table <- make_refugee_weight_table(input_df_ref = df_ref, 
-                                              input_refugee_pop = df_ref_pop)
-df_ref_with_weights <- df_ref %>% 
-  left_join(ref_weight_table, by = "strata")
-
-# host weights
-host_weight_table <- make_host_weight_table(input_df_host = df_host, 
-                                            input_host_pop = df_host_pop)
-df_host_with_weights <- df_host %>% 
-  left_join(host_weight_table, by = "strata")
 
 # set up design objects
 
-ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights )
-host_svy <- as_survey(.data = df_host_with_weights, strata = strata, weights = weights )
+ref_svy_protection_risky_places <- as_survey(.data = df_ref_with_weights_protection_risky_places)
+host_svy_protection_risky_places <- as_survey(.data = df_host_with_weights_protection_risky_places)
 
 
-df_protection_risky_places_analysis <- analysis_support(input_df_cleaned = df_protection_risky_places,
-                                                        input_df_ref_pop = df_ref_pop,
-                                                        input_df_host_pop = df_host_pop,
+df_protection_risky_places_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy_protection_risky_places,
+                                                                              input_host_svy = host_svy_protection_risky_places,
                                                         input_dap = dap %>% filter(variable %in% c("places_where_children_are_mostly_at_risk")))
 
 # children_perform_economic_labour_info: prepare data and create survey ------------------------------------------------
