9cd128dbbb4493fed74ec7672f684e7b04bfd6b3
Anthony Twesigye <twesigye10@gmail.com>
2022-04-06 09:41:12 +0300

    children_perform_economic_labour_info: prepare data and create survey

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -95,7 +95,7 @@ df_children_perform_domestic_chores_info_analysis <- analysis_support_after_surv
                                                                                             input_host_svy = host_svy_children_perform_domestic_chores, 
                                                                       input_dap = dap %>% filter(variable %in% c("hrs_child_perfoms_domestic_chores")))
 
-# main: prepare data and create survey ------------------------------------------------
+# protection_risky_places: prepare data and create survey ------------------------------------------------
 
 df_with_composites <- create_composite_indicators_cpa_caregiver(input_df = df_cleaned) %>% 
   mutate(strata = case_when(status == "refugee" ~ paste0(i.refugee_settlement, "_refugee"),
@@ -136,46 +136,35 @@ df_protection_risky_places_analysis <- analysis_support(input_df_cleaned = df_pr
                                                         input_df_host_pop = df_host_pop,
                                                         input_dap = dap %>% filter(variable %in% c("places_where_children_are_mostly_at_risk")))
 
-# main: prepare data and create survey ------------------------------------------------
+# children_perform_economic_labour_info: prepare data and create survey ------------------------------------------------
 
-df_with_composites <- create_composite_indicators_cpa_caregiver(input_df = df_cleaned) %>% 
-  mutate(strata = case_when(status == "refugee" ~ paste0(i.refugee_settlement, "_refugee"),
-                            status == "host_community" ~ paste0(i.region,"_host"),
-                            TRUE ~ status
-  ))
+df_with_composites_children_perform_economic_labour_info <- create_composite_indicators_cpa_caregiver(input_df = df_cleaned)
 
 # split data into host and refugee
 
-df_ref <- df_with_composites %>% 
+df_ref_children_perform_economic_labour_info <- df_with_composites_children_perform_economic_labour_info %>% 
   filter(status == "refugee")
 
-df_host <- df_with_composites %>% 
+df_host_children_perform_economic_labour_info <- df_with_composites_children_perform_economic_labour_info %>% 
   filter(status == "host_community")
 
-# create weights
+# attach weights
 
-# refugee weights
-ref_weight_table <- make_refugee_weight_table(input_df_ref = df_ref, 
-                                              input_refugee_pop = df_ref_pop)
-df_ref_with_weights <- df_ref %>% 
-  left_join(ref_weight_table, by = "strata")
+df_ref_with_weights_children_perform_economic_labour_info <- df_ref_children_perform_economic_labour_info %>% 
+  left_join(df_ref_with_weights %>% select(uuid, strata, weights), by = "uuid")
 
-# host weights
-host_weight_table <- make_host_weight_table(input_df_host = df_host, 
-                                            input_host_pop = df_host_pop)
-df_host_with_weights <- df_host %>% 
-  left_join(host_weight_table, by = "strata")
+df_host_with_weights_children_perform_economic_labour_info <- df_host_children_perform_economic_labour_info %>% 
+  left_join(df_host_with_weights %>% select(uuid, strata, weights), by = "uuid")
 
 # set up design objects
 
-ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights )
-host_svy <- as_survey(.data = df_host_with_weights, strata = strata, weights = weights )
+ref_svy_children_perform_economic_labour_info <- as_survey(.data = df_ref_with_weights_children_perform_economic_labour_info, strata = strata, weights = weights )
+host_svy_children_perform_economic_labour_info <- as_survey(.data = df_host_with_weights_children_perform_economic_labour_info, strata = strata, weights = weights )
 
 
-df_children_perform_economic_labour_info_analysis <- analysis_support(input_df_cleaned = df_children_perform_economic_labour_info,
-                                                                      input_df_ref_pop = df_ref_pop,
-                                                                      input_df_host_pop = df_host_pop,
-                                                                      input_dap = dap %>% filter(variable %in% c("hrs_child_perfoms_econ_labour")))
+df_children_perform_economic_labour_info_analysis <- analysis_support_after_survey_creation(input_ref_svy = ref_svy_children_perform_economic_labour_info,
+                                                                                            input_host_svy = host_svy_children_perform_economic_labour_info, 
+                                                                                            input_dap = dap %>% filter(variable %in% c("hrs_child_perfoms_econ_labour")))
 
 # merge analysis ----------------------------------------------------------
 
